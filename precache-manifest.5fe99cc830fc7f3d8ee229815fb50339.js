896f35342bcf770162e49c033d9fd16beeee5c0c
bogdanovaO <olya@MacBook-Pro-Olya.local>
2020-08-11 20:00:41 +0300

    Updates

---
+++ precache-manifest.5fe99cc830fc7f3d8ee229815fb50339.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5692ce3f97f945796a9737ab2e208b",
+    "url": "/github-dashboard/index.html"
+  },
+  {
+    "revision": "20484c31aecfc86415c1",
+    "url": "/github-dashboard/static/css/main.e4789079.chunk.css"
+  },
+  {
+    "revision": "3be78b876456ae292236",
+    "url": "/github-dashboard/static/js/2.679cb381.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-dashboard/static/js/2.679cb381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20484c31aecfc86415c1",
+    "url": "/github-dashboard/static/js/main.a6b156fe.chunk.js"
+  },
+  {
+    "revision": "9d40d30faad0fe2eb936",
+    "url": "/github-dashboard/static/js/runtime-main.176d9147.js"
+  },
+  {
+    "revision": "440640e161adf163d4d6744e4c529292",
+    "url": "/github-dashboard/static/media/gitIcon.440640e1.svg"
+  },
+  {
+    "revision": "2990891a9d0efb21ec0bc978c2eb12bc",
+    "url": "/github-dashboard/static/media/starIcon.2990891a.svg"
+  }
+]);
